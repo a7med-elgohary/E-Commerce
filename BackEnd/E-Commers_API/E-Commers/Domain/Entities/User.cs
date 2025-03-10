a068aabf15ee89422c2515a80036e2e7602de1ba
ahmed <ahmedelgoharyy7@gmail.com>
2025-03-10 16:00:48 +0200

    Add SQL Folder & add User as class

---
+++ BackEnd/E-Commers_API/E-Commers/Domain/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commers.Domain.Entities
+{
+    public class User
+    {
+        public User()
+        {
+            
+        }
+
+        public User(int id, string name, string email, string password, string? phoneNumber = null, string? address = null, bool isActive = false)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+            PhoneNumber = phoneNumber;
+            Address = address;
+            IsActive = isActive;
+        }
+
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Addess { get; set; }
+        public bool IsActive { get; set; } = False; 
+        
+    }
+}
